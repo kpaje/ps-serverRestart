7808d32f5ae4e5772feb6c5d6f045001f9556517
kpaje <kelvinmembership@gmail.com>
2020-02-19 20:32:17 -0800

    added verification for member of admin group

---
+++ verifyDomainAdmin.psm1
@@ -0,0 +1,10 @@
+$CurrentUser = $env:UserName | Get-ADUser
+$DomainAdminGroupMembers = Get-ADGroupMember "ITSTechs" | Select-Object SamAccountName
+$CurrentUserinDomainAdminGroup = $DomainAdminGroupMembers.SamAccountName -contains $CurrentUser.SamAccountName
+      if ($CurrentUserinDomainAdminGroup -eq $false){
+          Write-Host "This script must be run with Domain Administrator credentials" -ForegroundColor Red
+          Write-Host "Please open PowerShell with Domain Administrator credentials and run script again" -ForegroundColor Red
+          break
+      } else {
+        Write-Host $CurrentUser.SamAccountName "is a verified AD Member of KCLS\ITSTechs" -ForegroundColor Green
+      }
