2afdfdd99bb12551b5cb1868af72526512906d62
kpaje <kelvinmembership@gmail.com>
2020-02-22 03:29:53 -0800

    added variables for resuability and open-source friendly

--- promptCredentials.psm1
+++ promptCredentials.psm1
@@ -1,6 +1,7 @@
 ﻿function promptCredentials {
     $username = Read-Host "INPUT USERNAME"
     $password = Read-Host "INPUT PASSWORD" -AsSecureString
+    $DOMAIN = 'KCLS'
     $CREDENTIALS = New-Object -typename System.Management.Automation.PSCredential `
         -argumentlist $username, $password
 
@@ -11,7 +12,7 @@
     $domain = New-Object System.DirectoryServices.DirectoryEntry($CurrentDomain, $AdUser, $AdPassword)
 
     Write-Host "Verifying..." -ForegroundColor Magenta
-    Write-Host "UserName - KCLS\$adUser" -ForegroundColor Magenta
+    Write-Host "UserName - $DOMAIN\$adUser" -ForegroundColor Magenta
     Write-Host "Password - $password" -ForegroundColor Magenta
 
     if ($domain.Username -eq $null) {
--- verifyDomainAdmin.psm1
+++ verifyDomainAdmin.psm1
@@ -1,13 +1,16 @@
+$GROUPNAME =  Get-Content -Path ./groupName.txt
+$DOMAIN = 'KCLS'
+
 function verifyDomainAdmin {
     $CurrentUser = $env:UserName | Get-ADUser
-    $DomainAdminGroupMembers = Get-ADGroupMember "ITSTechs" | Select-Object SamAccountName
+    $DomainAdminGroupMembers = Get-ADGroupMember $GROUPNAME | Select-Object SamAccountName
     $CurrentUserinDomainAdminGroup = $DomainAdminGroupMembers.SamAccountName -contains $CurrentUser.SamAccountName
           if ($CurrentUserinDomainAdminGroup -eq $false){
               Write-Host "This script must be run with Domain Administrator credentials" -ForegroundColor Red
               Write-Host "Please open PowerShell with Domain Administrator credentials and run script again" -ForegroundColor Red
               break
           } else {
-            Write-Host $CurrentUser.SamAccountName "is a verified AD Member of KCLS\ITSTechs" -ForegroundColor Green
+            Write-Host $CurrentUser.SamAccountName "is a verified AD Member of $DOMAIN\$GROUPNAME" -ForegroundColor Green
             Write-Host ''
           }
 }
