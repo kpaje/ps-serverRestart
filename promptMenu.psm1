d83e6a6c9d6bbe9a427b5dba32dbc167d4b11c9a
kpcruz <kelvinmembership@gmail.com>
2020-02-25 15:09:13 -0800

    MenuItem class created for future items

--- promptMenu.psm1
+++ promptMenu.psm1
@@ -2,14 +2,35 @@
     $TITLE = Write-Host "KCLS ITS SERVER RESTART SCRIPT" -ForegroundColor DarkYellow
     $INFO = Write-Host "Select one server or all servers" -ForegroundColor Blue
 
-    $ONESERVER = New-Object System.Management.Automation.Host.ChoiceDescription "&ONE_Server", `
-        "Reboots ONE jqe server"
-    $ALLSERVERS = New-Object System.Management.Automation.Host.ChoiceDescription "&ALL_Servers", `
-        "Reboots ALL jqe servers"
-    $QUIT = New-Object System.Management.Automation.Host.ChoiceDescription "&Quit", `
-        "Exit Application"
 
-    $OPTIONS = [System.Management.Automation.Host.ChoiceDescription[]] @($ONESERVER, $ALLSERVERS, $QUIT)
+    Class MenuItem {
+        [String]$itemName
+        [String]$itemDescription
+
+        # constructor
+        MenuItem([String]$itemName, [String]$itemDescription) {
+            $this.itemName = $itemName
+            $this.itemDescription = $itemDescription
+        }
+
+        [MenuItem]Create($itemName, $itemDescription) {
+            return New-Object System.Management.Automation.Host.ChoiceDescription $itemName, `
+                $itemDescription
+        }
+    }
+    $OneServer = [MenuItem]::Create("&ONE_Server", "Reboots ONE jqe server")
+    $AllServers = [MenuItem]::Create("&ALL_Servers", "Reboots ALL jqe servers")
+    $Quit = [MenuItem]::Create("&Quit", "Exit Application")
+
+
+    # $ONESERVER = New-Object System.Management.Automation.Host.ChoiceDescription "&ONE_Server", `
+    #     "Reboots ONE jqe server"
+    # $ALLSERVERS = New-Object System.Management.Automation.Host.ChoiceDescription "&ALL_Servers", `
+    #     "Reboots ALL jqe servers"
+    # $QUIT = New-Object System.Management.Automation.Host.ChoiceDescription "&Quit", `
+    #     "Exit Application"
+
+    $OPTIONS = [System.Management.Automation.Host.ChoiceDescription[]] @($OneServer, $AllServers, $Quit)
     [int]$DEFAULTCHOICE = 2 #&Quit
     $prompt = $host.UI.PromptForChoice($TITLE , $INFO , $OPTIONS, $DEFAULTCHOICE)
     switch ($prompt) {
